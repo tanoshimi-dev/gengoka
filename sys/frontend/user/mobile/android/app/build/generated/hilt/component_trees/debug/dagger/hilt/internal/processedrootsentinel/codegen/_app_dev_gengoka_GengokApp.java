8e80815f63a4f254918bfd48005dcb4d5deb261c
mitaki <mitakik@mitakinoMacBook-Pro.local>
2026-02-02 06:31:50 +0900

    add gitignore

--- sys/frontend/user/mobile/android/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_app_dev_gengoka_GengokApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "app.dev.gengoka.GengokApp"
-)
-public final class _app_dev_gengoka_GengokApp {
-}
